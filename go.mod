47beaa84f6b110d1a0615cf3a1ad9cb8923d86ce
James Hrisho <james.hrisho@gmail.com>
2019-10-12 22:01:10 -0400

    working code

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/securingsincity/myq-webhook
+
+go 1.12
+
+require (
+	github.com/go-chi/chi v4.0.2+incompatible
+	github.com/joeshaw/myq v0.0.0-20190604014436-e399c9c85d42
+)
--- main.go
+++ main.go
@@ -1,14 +1,41 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"os"
 
+	"github.com/go-chi/chi"
 	"github.com/joeshaw/myq"
 )
 
 func main() {
-	log.Println("LET's Do this")
-	&myq.Session{}
-	http.ListenAndServe(":8080", nil)
+	s := &myq.Session{}
+	s.Username = os.Getenv("MYQ_EMAIL")
+	s.Password = os.Getenv("MYQ_PASSWORD")
+	deviceID := os.Getenv("MYQ_DEVICEID")
+	port := os.Getenv("MYQ_WEBHOOK_PORT")
+	s.Brand = "liftmaster"
+	r := chi.NewRouter()
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		log.Print("We're running")
+	})
+
+	r.Post("/{id}/open", func(w http.ResponseWriter, r *http.Request) {
+		s.Login()
+
+		s.SetDeviceState(deviceID, myq.StateOpen)
+		w.Write([]byte(fmt.Sprintf("opening:%s", deviceID)))
+		log.Printf("opening device id %s", deviceID)
+	})
+	r.Post("/{id}/close", func(w http.ResponseWriter, r *http.Request) {
+		s.Login()
+
+		s.SetDeviceState(deviceID, myq.StateClosed)
+		w.Write([]byte(fmt.Sprintf("closing:%s", deviceID)))
+		log.Printf("closing device id %s", deviceID)
+	})
+	log.Printf("Bound to port %s", port)
+	http.ListenAndServe(fmt.Sprintf(":%s", port), r)
 }
